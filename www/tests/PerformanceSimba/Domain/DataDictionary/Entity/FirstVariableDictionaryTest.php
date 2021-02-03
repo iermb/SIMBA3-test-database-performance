3834af22d375ed706566ba5895ccd10012ddb7fd
Jordi Llobet <jordi.llobet@uab.cat>
2021-02-03 14:24:38 +0100

    Add tests OneVariableDataTest and FirstVariableDictionary

---
+++ www/tests/PerformanceSimba/Domain/DataDictionary/Entity/FirstVariableDictionaryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\PerformanceSimba\Domain\DataDictionary\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstVariableDictionaryTest extends TestCase
+{
+    private FirstVariableDictionary $firstVariableDictionary;
+
+    /** @test */
+    public function shouldFirstVariableDictionaryReturnIdAndName(): void
+    {
+        $this->givenAFirstVariableDictionary();
+        $this->thenReturnIdAndName();
+    }
+
+    private function givenAFirstVariableDictionary(): void
+    {
+        $this->firstVariableDictionary = new FirstVariableDictionary(2, "Test name dictionary");
+    }
+
+    private function thenReturnIdAndName(): void
+    {
+        $this->assertEquals(2, $this->firstVariableDictionary->id());
+        $this->assertEquals("Test name dictionary", $this->firstVariableDictionary->name());
+    }
+}
---
+++ www/tests/PerformanceSimba/Domain/DataDictionary/Entity/OneVariableDataTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\PerformanceSimba\Domain\DataDictionary\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class OneVariableDataTest extends TestCase
+{
+    private OneVariableData $oneVariableData;
+
+    /** @test */
+    public function shouldOneVariableDataReturnVariableIdAndValue(): void
+    {
+        $this->givenAnOneVariableData();
+        $this->thenReturnVariableIdAndValue();
+    }
+
+    private function givenAnOneVariableData(): void
+    {
+        $this->oneVariableData = new OneVariableData(2, 45.6);
+    }
+
+    private function thenReturnVariableIdAndValue(): void
+    {
+        $this->assertEquals(2, $this->oneVariableData->variableId());
+        $this->assertEquals(45.6, $this->oneVariableData->value());
+    }
+}
